0704b007c4f3fa3ba2452bc331171a148de3c627
ContactApp <youssefmohammed06266@gmail.com>
2025-02-19 23:12:46 +0200

    finish login with google

---
+++ android/app/src/main/kotlin/com/example/event2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.event2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ dot/android/app/src/main/kotlin/com/example/dot/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
